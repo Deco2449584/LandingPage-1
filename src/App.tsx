b8a86c59596713157243e2d4ad1313974a982b7c
DomoCodeTech <domocodetech@gmail.com>
2024-09-02 14:18:06 -0500

    navbar1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <>
-      <div className="flex items-center justify-center min-h-screen bg-gray-100">
-        <h1 className="text-4xl font-bold">Hello World</h1>
-      </div>
+     <Navbar />
     </>
   )
 }
