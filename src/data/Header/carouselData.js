5e10c8b4c0d5bde3a173123ffe7499fe9fb97f69
DomoCodetech <156954210+DomoCodeTech@users.noreply.github.com>
2024-09-12 19:34:40 -0500

    Update carouselData.js
    firebase

--- src/data/Header/carouselData.js
+++ src/data/Header/carouselData.js
@@ -1,44 +1,42 @@
-import { Video } from "lucide-react";
-
 export const carouselData = [
-    {
-      id: 1,
-      video: '../../../public/video/world.mp4',
-      audio: '../../../public/audio/audio1.mp3', // Add audio file path
-      thumbnail: '../../../public/thumbnails/img1.jpg', 
-      author: 'LUNDEV',
-      title: 'DESIGN SLIDER',
-      topic: 'ANIMAL',
-      description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?',
-    },
-    {
-      id: 2,
-      video:'../../../public/video/video2.mp4',
-      audio: '../../../public/audio/audio1.mp3', // Add audio file path
-      thumbnail:'../../../public/thumbnails/img2.jpg', 
-      author: 'LUNDEV',
-      title: 'NATURE LOVE',
-      topic: 'SCENERY',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    },
-    {
-      id: 3,
-      video:'../../../public/video/video3.mp4',
-      audio: '../../../public/audio/audio1.mp3', // Add audio file path
-      thumbnail:'../../../public/thumbnails/img3.jpg',
-      author: 'LUNDEV',
-      title: 'WILD LIFE',
-      topic: 'SAFARI',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      id: 4,
-      video:'../../../public/video/video4.mp4',
-      audio: '../../../public/audio/audio1.mp3', // Add audio file path
-      thumbnail:'../../../public/thumbnails/img4.jpg', 
-      author: 'LUNDEV',
-      title: 'URBAN JUNGLE',
-      topic: 'CITY',
-      description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
-    }
+  {
+    id: 1,
+    video: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/video1.mp4?alt=media&token=f1fd899b-3c45-46a9-97dd-e393d4ed7005',
+    audio: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/audio1.mp3?alt=media&token=25c3b82c-99c6-4203-a22c-3c53175c3fa2',
+    thumbnail: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/img1.jpg?alt=media&token=ed1368e0-4b9d-4d3c-946a-475fa6efa11f', 
+    author: 'LUNDEV',
+    title: 'DESIGN SLIDER',
+    topic: 'ANIMAL',
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut sequi, rem magnam nesciunt minima placeat, itaque eum neque officiis unde, eaque optio ratione aliquid assumenda facere ab et quasi ducimus aut doloribus non numquam. Explicabo, laboriosam nisi reprehenderit tempora at laborum natus unde. Ut, exercitationem eum aperiam illo illum laudantium?',
+  },
+  {
+    id: 2,
+    video: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/video2.mp4?alt=media&token=5faf25b6-41db-4494-8f55-e52bfa1c6fd1',
+    audio: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/audio1.mp3?alt=media&token=25c3b82c-99c6-4203-a22c-3c53175c3fa2',
+    thumbnail: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/img2.jpg?alt=media&token=79f02690-1115-421d-b95d-b92eeab5bdee', 
+    author: 'LUNDEV',
+    title: 'NATURE LOVE',
+    topic: 'SCENERY',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  {
+    id: 3,
+    video: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/video3.mp4?alt=media&token=fd893c44-b32c-4acf-8b8e-06952e146824',
+    audio: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/audio1.mp3?alt=media&token=25c3b82c-99c6-4203-a22c-3c53175c3fa2',
+    thumbnail: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/img3.jpg?alt=media&token=2d008780-fa48-4022-9d92-56c099986465',
+    author: 'LUNDEV',
+    title: 'WILD LIFE',
+    topic: 'SAFARI',
+    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+  },
+  {
+    id: 4,
+    video: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/video4.mp4?alt=media&token=c91d177e-fbf9-4d6b-8216-c02a673ec2a3',
+    audio: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/audio1.mp3?alt=media&token=25c3b82c-99c6-4203-a22c-3c53175c3fa2',
+    thumbnail: 'https://firebasestorage.googleapis.com/v0/b/domocodetech-833cb.appspot.com/o/img4.jpg?alt=media&token=99e5a103-0fe1-4ba8-b14f-c8b60726a1a5', 
+    author: 'LUNDEV',
+    title: 'URBAN JUNGLE',
+    topic: 'CITY',
+    description: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+  }
 ];
